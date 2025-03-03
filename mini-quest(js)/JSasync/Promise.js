32bf93dabf0a6bb4c75bdd887bdf282d5af852bf
kdaehun00 <kdaehun00@naver.com>
2025-03-03 23:30:59 +0900

    Delete mini-quest(js) directory

--- mini-quest(js)/JSasync/Promise.js
+++
@@ -1,8 +0,0 @@
-const myFirstPromise = new Promise((resolve, reject) => {
-    const message = 'Hello, Promise';
-    resolve(message);
-})
-
-myFirstPromise.then(message => {
-    console.log(message);
-})
